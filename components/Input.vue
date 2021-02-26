17836e52aba4d87a7f102ae2b8ba2f8274739adb
Renan <renanhc96@gmail.com>
2021-02-26 14:01:14 -0300

    Creating of the main components (see description)
    - Components: Input, Header, Nav Button
    - Pages: Root, Painting The Room
    - Store: Initializing 
    - NewPackage: Vuex Persisted State

---
+++ components/Input.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-form-group
+      id="fieldset-1"
+      :description="`Let us know the ${typeDimension}.`"
+      label="Enter the value"
+      label-for="input-user"
+      valid-feedback="Okay, i understood!"
+      :invalid-feedback="invalidFeedback"
+      :state="state"
+    >
+      <b-form-input id="input-user" v-model="value" :state="state" trim />
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  },
+  computed: {
+    state () {
+      return this.value >= 1 && this.value <= 15
+    },
+    invalidFeedback () {
+      if (this.value.length > 0) {
+        return 'Please, the dimension must be between 1 meter and 15 meters.'
+      }
+      return 'Please enter something.'
+    }
+  }
+}
+</script>
---
+++ components/NavButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink class="text-light" :to="to">
+    <b-button>
+      {{ title }}
+    </b-button>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  name: 'NavigationButton',
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  b-button {
+    background-color: #3a9bc8;
+  }
+</style>
